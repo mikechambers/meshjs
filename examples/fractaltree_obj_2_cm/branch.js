831481b5ded1ed1bc07383075e342e4a39a84161
Mike Chambers <mikechambers@gmail.com>
2018-05-28 20:28:09 -0700

    Branch of previous fractal project

---
+++ examples/fractaltree_obj_2_cm/branch.js
@@ -0,0 +1,147 @@
+import Color from "../../lib/color.js";
+import { random } from "../../lib/math.js";
+import Vector from "../../lib/vector.js";
+import { pointOnCircle, pointOnLine } from "../../lib/utils.js";
+
+export default class Branch {
+  constructor(start, end) {
+    this._start = start;
+    this._end = end;
+
+    this._children = [];
+
+    this._leafColor = Color.WHITE;
+    this._leafRadius = 6;
+    this._nodeRadius = 3;
+    this._branchColor = Color.WHITE;
+
+    this._options = undefined;
+
+    this._maxRotationAngle = Math.PI / 6;
+  }
+
+  updateLeafColor(color) {
+    this._leafColor = color;
+
+    for (let c of this._children) {
+      c.updateLeafColor(color);
+    }
+  }
+
+  set start(position) {
+    this._start = position;
+  }
+
+  get start() {
+    return this._start;
+  }
+
+  set options(options) {
+    this._leafColor = options.leafColor;
+    this._leafRadius = options.leafRadius;
+    this._nodeRadius = options.nodeRadius;
+    this._branchColor = options.branchColor;
+
+    this._options = options;
+
+    if (this.hasChildren) {
+      for (let b of this._children) {
+        b.options = options;
+      }
+    }
+  }
+
+  get hasChildren() {
+    return this._children != undefined && this._children.length > 0;
+  }
+
+  set leafColor(color) {
+    this._leafColor = color;
+  }
+
+  set nodeRadius(radius) {
+    this._nodeRadius = radius;
+  }
+
+  set leafRadius(radius) {
+    this._leafRadius = radius;
+  }
+
+  set branchColor(color) {
+    this._branchColor = color;
+  }
+
+  jitter() {
+    this._end.add(Vector.createRandom(1));
+
+    for (let b of this._children) {
+      b.start = this._end;
+      b.jitter();
+    }
+  }
+
+  draw(ctx) {
+    if (this.hasChildren) {
+      for (let b of this._children) {
+        b.draw(ctx);
+      }
+    }
+
+    ctx.strokeStyle = this._branchColor.toRGBA();
+    ctx.lineWidth = 1.0;
+
+    let r = this.hasChildren ? this._nodeRadius : this._leafRadius;
+    let lineDestination = this._end;
+
+    if (!this.hasChildren) {
+      lineDestination = pointOnLine(this._end, this._start, r);
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(this._start.x, this._start.y);
+    ctx.lineTo(lineDestination.x, lineDestination.y);
+    ctx.stroke();
+
+    ctx.fillStyle = this.hasChildren
+      ? Color.WHITE.toRGBA()
+      : this._leafColor.toRGBA();
+
+    ctx.beginPath();
+    ctx.arc(this._end.x, this._end.y, r, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  get length() {
+    return this._start.distance(this._end);
+  }
+
+  spawn() {
+    if (this.hasChildren) {
+      for (let b of this._children) {
+        b.spawn();
+      }
+      return;
+    }
+
+    //let heading = Vector.subtract(this._end, this._start).heading;
+
+    let b1 = this.createChild(random(0, this._maxRotationAngle));
+    let b2 = this.createChild(-random(0, this._maxRotationAngle));
+
+    this._children.push(b1);
+    this._children.push(b2);
+  }
+
+  createChild(angle) {
+    let dir = Vector.subtract(this._end, this._start);
+    dir.rotate(angle);
+    dir.multiply(random(0.5, 0.8));
+
+    let end = Vector.add(this._end, dir);
+    let b = new Branch(this._end, end);
+    b.options = this._options;
+
+    return b;
+  }
+}
---
+++ examples/fractaltree_obj_2_cm/main.js
@@ -0,0 +1,173 @@
+/**
+	Mike Chambers
+	https://github.com/mikechambers
+	http://www.mikechambers.com
+
+	Released under an MIT License
+	Copyright Mike Chambers 2018
+**/
+
+import meshjs from "../../lib/mesh.js";
+import Vector from "../../lib/vector.js";
+import Circle from "../../lib/circle.js";
+import Color from "../../lib/color.js";
+import { map } from "../../lib/math.js";
+import Branch from "./branch.js";
+import * as utils from "../../lib/utils.js";
+import ColorPalette, { randomColorPallete } from "../../lib/colorpallete.js";
+import Gradient, { gradientFromName } from "../../lib/gradient.js";
+
+/************ CONFIG **************/
+
+const config = {
+  /**** required for mesh lib ******/
+
+  //name of container that generated canvas will be created in
+  PARENT_ID: "canvas_container",
+
+  //prints config to console at init
+  PRINT_CONFIG_ON_START: true,
+
+  //app name, used for saving files
+  PROJECT_NAME: meshjs.getProjectName(),
+
+  //whether we proxy and capture canvas calls so we can spit out svg
+  //svg output currently not implimented
+  CAPTURE_SVG: false,
+
+  //whether to output debug information (currently just for)
+  //canvas rendering.
+  ENABLE_DEBUG: false,
+
+  //Dimensions that canvas will be rendered at
+  RENDER_HEIGHT: 1080, //2436,
+  RENDER_WIDTH: 1080, //1125,
+
+  //Max dimension canvas will be display at on page
+  //note, exact dimension will depend on RENDER_HEIGHT / width and
+  //ratio to these properties.
+  //Canvas display will be scaled to maintain aspect ratio
+  MAX_DISPLAY_HEIGHT: 640,
+  MAX_DISPLAY_WIDTH: 640,
+
+  //background color of html page
+  BACKGROUND_COLOR: "#000000",
+
+  //background color for display and offscreen canvas
+  CANVAS_BACKGROUND_COLOR: "#222222",
+
+  //whether a single frame is rendered, or draw is called based on FPS setting
+  ANIMATE: true,
+  FPS: 60,
+
+  //Where video of canvas is recorded
+  RECORD_VIDEO: true,
+
+  //whether canvas should be cleared prior to each call to draw
+  CLEAR_CANVAS: true,
+
+  KEY_COMMANDS: {
+    g: "Add nodes",
+    b: "Cycle through colors"
+  },
+
+  /*********** APP Specific Settings ************/
+
+  LEAF_COLOR: "#00FF00",
+  LEAF_RADIUS: 8,
+  NODE_RADIUS: 3,
+  LEAF_OPACITY: 0.9,
+  BRANCH_COLOR: "#333333", //"#EEEEEE",
+  GRADIENT_NAME: "Bora Bora",
+  START_GENERATIONS: 10,
+  JITTER: true
+};
+
+/************** GLOBAL VARIABLES ************/
+
+//hot pink : 255,20,147
+
+let ctx;
+let bounds;
+
+let circle;
+let angle = 3 * Math.PI / 4;
+
+let mousePosition = new Vector();
+
+let colorPallete;
+let gradient;
+
+/*************** CODE ******************/
+
+let root;
+let leafColor;
+const init = function(canvas) {
+  ctx = canvas.context;
+  bounds = canvas.bounds;
+
+  gradient = gradientFromName(
+    config.GRADIENT_NAME,
+    bounds,
+    Gradient.TOP_TO_BOTTOM
+  );
+  gradient.create();
+
+  colorPallete = randomColorPallete();
+
+  root = new Branch(
+    new Vector(bounds.center.x, bounds.height),
+    new Vector(bounds.center.x, bounds.height - bounds.height / 4)
+  );
+
+  let c = colorPallete.getRandomColor();
+  c.alpha = config.LEAF_OPACITY;
+
+  let options = {
+    leafColor: c,
+    leafRadius: config.LEAF_RADIUS,
+    nodeRadius: config.NODE_RADIUS,
+    branchColor: Color.fromHex(config.BRANCH_COLOR)
+  };
+
+  root.options = options;
+
+  for (let i = 0; i < config.START_GENERATIONS; i++) {
+    root.spawn();
+  }
+};
+
+const draw = function(canvas, frameCount) {
+  ctx.drawImage(
+    gradient.canvas,
+    bounds.x,
+    bounds.y,
+    bounds.width,
+    bounds.height
+  );
+
+  if (config.JITTER) {
+    root.jitter();
+  }
+  root.draw(ctx);
+};
+
+const updateColor = function() {
+  let c = colorPallete.getRandomColor();
+  c.alpha = config.LEAF_OPACITY;
+
+  root.updateLeafColor(c);
+};
+
+const keypress = function(event) {
+  if (event.key === "g") {
+    root.spawn();
+  } else if (event.key === "b") {
+    updateColor();
+  }
+};
+
+window.onload = function() {
+  meshjs.init(config, init, draw);
+  meshjs.listen(keypress);
+};
