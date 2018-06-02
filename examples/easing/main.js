e53aa8cb74b29f919ff4a8dd22dc8676758aba33
Mike Chambers <mikechambers@gmail.com>
2018-06-01 23:15:48 -0700

    Initial easing  / tween work and experimentatiom
    Messy and just playing around with structure and format.

---
+++ examples/easing/main.js
@@ -0,0 +1,78 @@
+/**
+	Mike Chambers
+	https://github.com/mikechambers
+	http://www.mikechambers.com
+
+	Released under an MIT License
+	Copyright Mike Chambers 2018
+**/
+
+import meshjs from "../../lib/mesh.js";
+import * as easing from "../../lib/motion/easing.js";
+import Circle from "../../lib/geometry/circle.js";
+import Vector from "../../lib/math/vector.js";
+
+/************ CONFIG **************/
+
+const config = {
+  //Dimensions that canvas will be rendered at
+  RENDER_HEIGHT: 1080,
+  RENDER_WIDTH: 1080,
+
+  //Max dimension canvas will be display at on page
+  //note, exact dimension will depend on RENDER_HEIGHT / width and
+  //ratio to these properties.
+  //Canvas display will be scaled to maintain aspect ratio
+  MAX_DISPLAY_HEIGHT: 640,
+  MAX_DISPLAY_WIDTH: 640,
+
+  //background color of html page
+  BACKGROUND_COLOR: "#EEEEEE",
+
+  //background color for display and offscreen canvas
+  CANVAS_BACKGROUND_COLOR: "#FAFAFA",
+
+  //Where video of canvas is recorded
+  RECORD_VIDEO: false,
+
+  /*
+    Custom keyboard commands for project. These will be printed in the console
+    at start. Commands should be in a format like so:
+    KEY_COMMANDS: {
+      g: "Add nodes",
+      b: "Cycle through colors"
+    },
+  */
+  KEY_COMMANDS: {}
+
+  /*********** APP Specific Settings ************/
+};
+
+let bounds;
+let circle;
+let cBounds;
+let tween;
+
+const init = function(context) {
+  bounds = meshjs.bounds;
+  cBounds = bounds.withPadding(20);
+
+  let radius = 20;
+  circle = new Circle(cBounds.x + radius, cBounds.center.y, radius);
+
+  circle.startPosition = circle.center.clone();
+  circle.destination = new Vector(cBounds.width - radius, cBounds.y);
+  tween = new easing.Tween(circle.startPosition.x, circle.destination.x, 5000);
+  tween.start();
+};
+
+const draw = function(context, frameCount) {
+  circle.center.x = tween.update();
+
+  cBounds.draw(context);
+  circle.draw(context);
+};
+
+window.onload = function() {
+  meshjs.init(config, init, draw);
+};
