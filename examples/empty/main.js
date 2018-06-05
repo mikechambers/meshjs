267fe4dd4995d53f4e8a28a14fa6755ff2a74eb9
Mike Chambers <mikechambers@gmail.com>
2018-06-04 18:21:03 -0700

    empty project to make it easy to profile core framework

---
+++ examples/empty/main.js
@@ -0,0 +1,48 @@
+/**
+	Mike Chambers
+	https://github.com/mikechambers
+	http://www.mikechambers.com
+
+	Released under an MIT License
+	Copyright Mike Chambers 2018
+**/
+
+import meshjs from "../../lib/mesh.js";
+
+/************ CONFIG **************/
+
+let config = {
+  //Dimensions that canvas will be rendered at
+  RENDER_HEIGHT: 1080,
+  RENDER_WIDTH: 1080,
+
+  //Max dimension canvas will be display at on page
+  //note, exact dimension will depend on RENDER_HEIGHT / width and
+  //ratio to these properties.
+  //Canvas display will be scaled to maintain aspect ratio
+  MAX_DISPLAY_HEIGHT: 640,
+  MAX_DISPLAY_WIDTH: 640,
+
+  //background color of html page
+  BACKGROUND_COLOR: "#EEEEEE",
+
+  //background color for display and offscreen canvas
+  CANVAS_BACKGROUND_COLOR: "#FAFAFA",
+
+  //whether video of canvas is recorded
+  RECORD_VIDEO: false
+};
+
+let bounds;
+
+//called when project is being initialized
+const init = function(context) {
+  bounds = meshjs.bounds;
+};
+
+//called once per frame (if config.ANIMATE is true)
+const draw = function(context, frameCount) {};
+
+window.onload = function() {
+  meshjs.init(config, init, draw);
+};
